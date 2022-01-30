3b37f47d6479498ce08bbe1f522d4b50694fc852
Asher Mathews <msasher123@gmail.com>
2022-01-30 20:17:32 +0530

    added some files

---
+++ cycle-5/no_of_characters_lines_words.java
@@ -0,0 +1,40 @@
+import java.io.*;
+
+
+class NoOfParts
+{
+	public static void main(String args[])
+	{
+		FileInputStream f1=null;
+		int i;
+		int chars=0,spaces=0,lines=0;
+		try
+		{
+			f1=new FileInputStream(args[0]);
+			do
+			{
+				i=f1.read();
+				if(i==(int)' ')spaces++;
+				if(i==(int)'\n')lines++;
+				chars++;
+
+			}while(i!=-1);
+
+		}catch(IOException e)
+		{
+			System.out.println("Error in Opening the file");
+		}finally
+		{
+			try
+			{
+				f1.close();
+			}catch(IOException e)
+			{
+				System.out.println("Error in closing the file");
+			}
+		}
+		System.out.println("NO of characters : "+chars);
+		System.out.println("NO of words : "+(spaces+1));
+		System.out.println("NO of lines : "+(lines+1));
+	}
+}
