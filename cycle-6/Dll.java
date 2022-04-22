3e4f60d5f35b890ecc620c32adf00f2daf7a9216
Sanjay <sanjaysj6282@gmail.com>
2022-04-22 08:58:04 +0530

    d

--- cycle-6/Dll.java
+++ cycle-6/Dll.java
@@ -3,11 +3,11 @@ import java.util.*;
 class Dll {
     public static void main(String args[]) {
         LinkedList<String> list = new LinkedList<String>();
-        list.add("Ajith");
-        list.add(0, "Mnakshi");
-        list.add("Dinoy");
-        list.add("Emil");
-        list.add("Jijo");
+        list.add("Alok");
+        list.add(0, "SanjaY");
+        list.add("Prince");
+        list.add("Joel");
+        list.add("Kishan");
         System.out.println(list + "\n");
         list.remove(4);
         list.remove("Emil");
